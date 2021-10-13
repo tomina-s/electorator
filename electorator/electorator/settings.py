1c4abb6d170faac90ca7a8106b5d13deef3043d1
Huvalk <akiraq@yandex.ru>
2021-10-13 16:54:33 +0300

    feat: vuejs

--- electorator/electorator/settings.py
+++ electorator/electorator/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'electorator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, '../frontend/build/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '../frontend/build/static')]
 
 AUTH_USER_MODEL = 'accounts.Account'
 
