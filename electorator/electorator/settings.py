02f7522da4c863a3f53c31b54b30175f9c913853
Anna <annyshka1997@yandex.ru>
2021-10-13 11:57:35 +0300

    make DB_test model and add model to settings

--- electorator/electorator/settings.py
+++ electorator/electorator/settings.py
@@ -33,14 +33,15 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = {
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'mainapp.apps.MainappConfig',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ electorator/mainapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db.models import Model
+
+
+def DB_test(Model):
+    pass
+
+
