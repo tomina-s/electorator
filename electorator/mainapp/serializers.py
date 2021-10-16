08533cb5b9e5600229d5af6d0c2f81ba7327bcc7
Svetlana <svetik.tomina@mail.ru>
2021-10-16 16:03:32 +0300

    add - serializer for Cendidate

--- electorator/mainapp/serializers.py
+++ electorator/mainapp/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 from .models import DB_test
+from .models import Candidate
+
 
 class DB_testSerializer(serializers.ModelSerializer):
     class Meta:
         pass
+
+
+class CandidateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Candidate
+        fields = '__all__'
+
