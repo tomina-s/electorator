f851b583294ba92656813bcd05c257eed4990f9d
Svetlana <svetik.tomina@mail.ru>
2021-10-18 23:17:30 +0300

    add - просмотр списка кандидатов

--- electorator/mainapp/api.py
+++ electorator/mainapp/api.py
@@ -1,12 +1,48 @@
 from .models import Candidate
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, response
 from .serializers import CandidateSerializer
+
 # мб еще импорты по сералайзеру должны быть и другие классы из сериалайзера
 
 
 class CandidateViewSet(viewsets.ModelViewSet):
-    queryset = Candidate.objects.all()
-    permission_classes = [
-        permissions.AllowAny
-    ]
-    serializer_class = CandidateSerializer
+
+    """
+    ViewSet для отображения данных по кандидатам.
+
+    Кандидат - чтение (модели).
+    list_of_candidats - чтение всех кандидатов на данных выборах.
+
+    user_list = CandidateViewSet.as_view({'get': 'list_of_candidats'})
+    user_detail = CandidateViewSet.as_view({'get': '***'})
+
+    """
+
+    def list_of_candidats(self, request):
+        queryset = Candidate.objects.all()
+        permission_classes = [
+            permissions.AllowAny
+        ]
+        #  что за переменная класса CandidateSerializer?
+        # здесь в апи или в сериалайзере прописывать какие поля Модели нужны для этого метода -
+        #     например: для list_of_candidats нужно только поле "name"
+        serializer_class = CandidateSerializer(queryset, many=True)
+        return response.Response(serializer_class.data)
+
+    def view_candidate_info(self, request):
+        queryset = Candidate.objects.all()
+        # надо разобраться что такое permission_classes
+        # permission_classes = [
+        #     permissions.AllowAny
+        # ]
+        serializer_class = CandidateSerializer(queryset, many=True)
+        return response.Response(serializer_class.data)
+
+
+#  examples
+# class UserViewSet(viewsets.ModelViewSet):
+#     """
+#     A viewset for viewing and editing user instances.
+#     """
+#     serializer_class = UserSerializer
+#     queryset = User.objects.all()
