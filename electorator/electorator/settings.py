37b11b173031c6293db1ecaf008865a756e0c24f
Huvalk <akiraq@yandex.ru>
2021-10-24 11:03:02 +0300

    fix(frontend)

--- electorator/electorator/settings.py
+++ electorator/electorator/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'configuration.apps.ConfigurationConfig'
+    'configuration.apps.ConfigurationConfig',
     'mainapp.apps.MainappConfig',
     'accounts.apps.AccountsConfig',
     'rest_framework',
