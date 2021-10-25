2bb3532ea3a1b5749be4bd3b152530c0b0e05ae5
Svetlana <svetik.tomina@mail.ru>
2021-10-25 19:45:54 +0300

    fix and add - request GET Protocol - add permissions

--- electorator/mainapp/api.py
+++ electorator/mainapp/api.py
@@ -67,19 +67,32 @@ class ProtocolViewSet(viewsets.ModelViewSet):
     """
 
     def list(self, request):
-        """GET /protocol - получить все протоколы, к которым пользователь имеет доступ."""
+        """GET /protocol - получить все протоколы, к которым пользователь имеет доступ.
+
+        :return два массива с протоколами 1 и второго типа по отдельности.
+        """
 
         permission_classes = [
-            permissions.AllowAny
+            permissions.IsAuthenticated
         ]
-        current_id_user = request.user.id
-        user_role = Role.objects.get(user_id=current_id_user)
-        # RoleSerializer
+        # получение юзера и user.id
+        user = request.user
+        # получение роли юзера
+        user_role = Role.objects.get(user_id=user.id)
 
-        queryset = Protocol1.objects.all()
-        serializer_class = Protocol1Serializer(queryset, many=True)
+        # получили уик
+        uik = Permission.objects.filter(purchaser=user)
+        # получение протоколов по этому уику
+        queryset_prot1 = Protocol1.onjects.filter(purchase=uik)
+        queryset_prot2 = Protocol2.onjects.filter(purchase=uik)
 
-        Protocol1.objects.filter(contractor=request.user.id)
+        serializer_class1 = Protocol1Serializer(queryset_prot1, meny=True)
+        serializer_class2 = Protocol2Serializer(queryset_prot2, meny=True)
+
+        # example:  Article - model
+        # queryset= Article.objects.all().filter(category=14).order_by('-created')[:10]
+        # serializer_class = Protocol1Serializer(queryset, many=True)
+
+        # Protocol1.objects.filter(contractor=request.user.id)
         # Protocol2.objects.filter(contractor=request.user.id)
-
-        return response.Response(serializer_class.data)
+        return response.Response(serializer_class1.data), response.Response(serializer_class2.data)
