80e92cc23a15c851141d563c5d67c7b233a2f5ad
Anna <annyshka1997@yandex.ru>
2021-10-29 12:51:36 +0300

    add api

--- electorator/mainapp/urls.py
+++ electorator/mainapp/urls.py
@@ -2,27 +2,29 @@ from django.urls import path, include
 from .api import (
     CandidateViewSet,
 
-    ProtocolSecondCreate, ProtocolsFirstList,
-    ProtocolsFirstListQuantity, PercVotersViewSet,
-    PresenceViewSet
-    ProtocolFirstCreate,
+    ProtocolsFirstList,
+    ProtocolsFirstListQuantity,
+    ProtocolFirst,
+
     ProtocolSecondCreate,
     AccountPermissionsViewSet, PresenceViewSet, PercVotersViewSet, TopPresenceViewSet, TopTikViewSet,
-    GeneralInfoViewSet,
+    GeneralInfoViewSet
 )
 
 urlpatterns = [
     path('protocols/first/', ProtocolFirst.as_view()),
     path('protocols/first/<int:prot_id>', ProtocolFirst.as_view()),
+    path('uiks/<int:uik_id>/candidates/short/list/', CandidateViewSet.as_view({'get': 'list_of_candidats'})),
+    path('uiks/<int:uik_id>/protocols/first/list/', ProtocolsFirstList.as_view()),
+    path('uiks/<int:uik_id>/protocols/first/quantity/', ProtocolsFirstListQuantity.as_view()),
     path('protocols/second/', ProtocolSecondCreate.as_view()),
+
     path('accounts/permission/', AccountPermissionsViewSet.as_view({'get': 'list_of_role_uik'})),
     path('presence/', PresenceViewSet.as_view()),
     path('votes/', PercVotersViewSet.as_view()),
     path('toppresence/', TopPresenceViewSet.as_view()),
     path('toptik/', TopTikViewSet.as_view()),
     path('generalinfo/', GeneralInfoViewSet.as_view())
-    path('uiks/<int:uik_id>/candidates/short/list/', CandidateViewSet.as_view({'get': 'list_of_candidats'})),
-    path('uiks/<int:uik_id>/protocols/first/list/', ProtocolsFirstList.as_view()),
-    path('uiks/<int:uik_id>/protocols/first/quantity/', ProtocolsFirstListQuantity.as_view())
+
 
 ]
