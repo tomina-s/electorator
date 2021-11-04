9f83bbc79275e8c73e428497d099162dbf36435c
Svetlana <s.tomina@madule.ru>
2021-11-04 18:46:25 +0300

    add - csv data for accounts table
    fix me

--- craete_csv.py
+++ craete_csv.py
@@ -6,82 +6,50 @@ import string
 
 '''
 1 прописать формат данных таблицы 
+ - создать дата фрейм
 
 - создать csv файл
+- загрузить в БД
 
 '''
 
 
-table_name_accounts = 'accounts_account'
-# прописать название колонок
-colums_accounts = ['password', 'last_login', 'name', 'username']
-
-# создать оъект pandas
-
-# для пароля
 def gen_password(length):
-    '''Создать случайную буквенно-цифровую строку длиной length.'''
+    """Создать случайную буквенно-цифровую строку длиной length."""
     letters_and_digits = string.ascii_letters + string.digits
     rand_string = ''.join(random.sample(letters_and_digits, length))
-    # print(rand_string)
     return rand_string
-    # rand_string = ''.join(random.sample(letters_and_digits, length))
-    # print("Alphanum Random string of length", length, "is:", rand_string)
 
 
-gen_password(6)
+def gen_string(length):
+    """Создать случайную буквеенную строку длиной length."""
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.choice(letters) for i in range(length))
+    return rand_string
 
-accounts_num = 10
-# нагенерировать рандомные (списки мб) данных
-password_list = []
-for _ in range(accounts_num):
-    password_list.append(gen_password(random.randint(5, 15)))
-# print('password_list', password_list)
 
-password_series = pd.Series([gen_password(random.randint(5, 15)) for _ in range(accounts_num)])
-print(password_series)
-# last_login
-name_series = pd.Series(['Alice' for _ in range(accounts_num)])
-username_series = pd.Series(['' for _ in range(accounts_num)])
+ACCOUNTS_NUM = 10
+table_name_accounts = 'accounts_account'
+
+# генерация рандомных данных
+password_series = pd.Series([gen_password(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
 # print("OUTPUT",  password_series)
-df = pd.DataFrame(password_series)
+# last_login - TODO: доделать генерацию полей - типа данных timestamp
+last_login = pd.Series([gen_string(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
+name_series = pd.Series([gen_string(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
+username_series = pd.Series([gen_string(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
+
+# создание DataFrame
+# colums_account = ['password', 'last_login', 'name', 'username']
+df = pd.DataFrame({'password': password_series,
+                   'last_login': name_series,
+                   'name': name_series,
+                   'username': username_series,
+})
 # print(df)
 
+# df.to_csv('accounts.csv', index=False)
+csv_data = df.to_csv(index=False)
+print('\nCSV String:\n', csv_data)
 
-# df = pd.DataFrame([[1,'Bob', 'Builder'],
-#                   [2,'Sally', 'Baker'],
-#                   [3,'Scott', 'Candle Stick Maker']],
-# columns=['id','name', 'occupation'])
-#
-# print(df)
-# #
-#
-# changes = pd.read_excel('Changes.xlsx', encoding='utf-8')
-# initial_list = []
-# final_list = []
-# initial_list_start = changes['initial'].tolist()
-# final_list_start = changes['should_be'].tolist()
-#
-# for elem in initial_list_start:
-# 	initial_list.append(elem.encode('utf-8'))
-# for elem in final_list_start:
-# 	final_list.append(elem.encode('utf-8'))
-#
-#
-#
-#
-# # 		read csv
-# df2 = pd.read_csv('megapolis_print.csv', sep=';') #, encoding='utf-8'
-# elem_name = df2['column'].tolist()
-# elem_name = elem_name[6:30]		#[6:392]
-# # print elem_name
-#
-#
-# df = pd.DataFrame(output_list)
-# print(df)
-# # изменить на запись в csv
-# # python dataframe pandas save to csv file format
-# # file_writer = csv.writer(w_file, delimiter = "\t")
-#
-# file_output = 'output_' + str(random.randint(1,1000)) + '.xlsx'
-# df.to_excel(file_output, 'Sheet1')
+# file_writer = csv.writer(w_file, delimiter = "\t")
