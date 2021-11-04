b5d1710a8d2015c0d20928ce4799f29edf64d3f0
Svetlana <s.tomina@madule.ru>
2021-11-04 18:43:53 +0300

    add - заполнение таблиц УИК и Кандидата

--- craete_csv.py
+++ craete_csv.py
@@ -30,43 +30,70 @@ def gen_string(length):
     return rand_string
 
 
-# path_to_DB = '***postgresql://username:password@localhost:5432/dbname'
-path_to_DB = 'postgresql://huvalk:1234@localhost:8000/database'
+path_to_DB = 'postgresql://postgres:***@***.ru:8001/electorator'
 ACCOUNTS_NUM = 10
 table_name_accounts = 'accounts_account'
 
 # генерация рандомных данных
 password_series = pd.Series([gen_password(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
-# last_login - TODO: доделать генерацию полей - типа данных timestamp
-last_login = pd.Series([gen_string(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
+# last_login - типа данных timestamp
+last_login_series = pd.Series(['2020-05-16 08:36:38' for _ in range(ACCOUNTS_NUM)])
 name_series = pd.Series([gen_string(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
 username_series = pd.Series([gen_string(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
 
 df_accounts = pd.DataFrame({'password': password_series,
-                            'last_login': name_series,
+                            'last_login': last_login_series,
                             'name': name_series,
                             'username': username_series,
 })
 
-
 ACCOUNTS_ROLE_NUM = 10
 table_name_accounts_role = 'accounts_role'
-
-user_series = pd.Series([random.randint(1, 10) for _ in range(ACCOUNTS_ROLE_NUM)])
-role_user_series = pd.Series([gen_string(random.randint(5, 10)) for _ in range(ACCOUNTS_ROLE_NUM)])
-df_account_role = pd.DataFrame({'user': user_series,
+USER_ROLE_MGIK = 'МГИК'
+USER_ROLE = ['ТИК', 'УИК']
+user_series = pd.Series([idx for idx in range(5, 5+ACCOUNTS_ROLE_NUM)])
+role_user_series = pd.Series(USER_ROLE[random.randint(0, 1)] for _ in range(ACCOUNTS_ROLE_NUM))
+df_account_role = pd.DataFrame({'user_id': user_series,
                                'role_user': role_user_series
                                 })
+# print('role_user_series\n', type(role_user_series))
+# print('df_account_role', df_account_role)
+engine = create_engine(path_to_DB)
+
+df_accounts.to_sql(table_name_accounts, engine, if_exists='append', index=False)  # if_exists='append' / replace
 
 engine = create_engine(path_to_DB)
-df_accounts.to_sql(table_name_accounts, engine)
+# print(engine)
+df_account_role.to_sql(table_name_accounts_role, engine, if_exists='replace', index=False)
 
-engine = create_engine(path_to_DB)
-df_account_role.to_sql(table_name_accounts_role, engine)
+CANDIDATE_NUM = 8
+table_name_candidate = 'mainapp_candidate'
+candidate_name_series = pd.Series(['Петров Иван Степанович', 'Лужков', 'Иванов', 'Собянин Сергей Семенович',
+                                   'Собянин Иван', 'Антонова', 'Срегеев', 'Романов',
+])
+party_series = pd.Series('Партия' for _ in range(CANDIDATE_NUM))
+info_series = pd.Series('Глава муниципального округа Таганский города Москвы' for _ in range(CANDIDATE_NUM))
+sim_votes_series = pd.Series(random.randint(500, 650) for _ in range(CANDIDATE_NUM))
+df_candidates = pd.DataFrame({'name': candidate_name_series,
+                              'party': party_series,
+                              'info': info_series,
+                              'sum_votes': sim_votes_series,
+                              'photo': pd.Series('file_name.jpg' for _ in range(CANDIDATE_NUM)),  # название файла .jpg
+                              })
+# print('df_candidates', df_candidates)
+df_candidates.to_sql(table_name_candidate, engine, if_exists='replace', index=False)
 
+table_name_uik = 'mainapp_uik'
 
-# print(df_accounts)
-# df_accounts.to_csv('accounts.csv', index=False)
-# csv_data = df_accounts.to_csv(index=False)
-# print('\nCSV String:\n', csv_data)
-
+UIK_NUM = 3628
+df_uik = pd.DataFrame({'num_uik': pd.Series(i for i in range(1, UIK_NUM+1)),
+                       'num_tik': pd.Series(1 for _ in range(1, UIK_NUM+1)),
+                       'population': pd.Series(random.randint(100, 300) for _ in range(UIK_NUM)),
+                       'sum_numb_votes_fin': pd.Series(0 for _ in range(UIK_NUM)),
+                       'presence': pd.Series(0 for _ in range(UIK_NUM)),
+                       'perc_final_bul': pd.Series(0 for _ in range(UIK_NUM)),
+                       'bad_form': pd.Series(random.randint(0, 5) for _ in range(UIK_NUM)),
+                       'update_time': pd.Series('2020-05-16 08:36:38' for _ in range(UIK_NUM)),
+                       })
+# print('df_uik\n',df_uik)
+df_candidates.to_sql(table_name_candidate, engine, if_exists='append', index=False)
