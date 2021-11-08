49bb250a6953519eb2a8fe90bd10910a0d815fdb
Svetlana <s.tomina@madule.ru>
2021-11-08 23:42:01 +0300

    подключение к через psycopg2 - заполнение таблицы Кандидатов

--- craete_csv.py
+++ craete_csv.py
@@ -4,6 +4,11 @@ import random
 import string
 from sqlalchemy import create_engine
 
+import psycopg2
+from psycopg2 import Error
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+
 '''
 Скрипт заполнения таблиц БД рандомными данными, походящими по типу.
 
@@ -30,10 +35,8 @@ def gen_string(length):
     return rand_string
 
 
-path_to_DB = 'postgresql://postgres:***@***.ru:8001/electorator'
 ACCOUNTS_NUM = 10
 table_name_accounts = 'accounts_account'
-
 # генерация рандомных данных
 password_series = pd.Series([gen_password(random.randint(5, 15)) for _ in range(ACCOUNTS_NUM)])
 # last_login - типа данных timestamp
@@ -47,6 +50,7 @@ df_accounts = pd.DataFrame({'password': password_series,
                             'username': username_series,
 })
 
+
 ACCOUNTS_ROLE_NUM = 10
 table_name_accounts_role = 'accounts_role'
 USER_ROLE_MGIK = 'МГИК'
@@ -58,17 +62,11 @@ df_account_role = pd.DataFrame({'user_id': user_series,
                                 })
 # print('role_user_series\n', type(role_user_series))
 # print('df_account_role', df_account_role)
-engine = create_engine(path_to_DB)
 
-df_accounts.to_sql(table_name_accounts, engine, if_exists='append', index=False)  # if_exists='append' / replace
-
-engine = create_engine(path_to_DB)
-# print(engine)
-df_account_role.to_sql(table_name_accounts_role, engine, if_exists='replace', index=False)
 
 CANDIDATE_NUM = 8
 table_name_candidate = 'mainapp_candidate'
-candidate_name_series = pd.Series(['Петров Иван Степанович', 'Лужков', 'Иванов', 'Собянин Сергей Семенович',
+candidate_name_series = pd.Series(['Петров Олег Степанович', 'Лужков', 'Иванов', 'Собянин Сергей Семенович',
                                    'Собянин Иван', 'Антонова', 'Срегеев', 'Романов',
 ])
 party_series = pd.Series('Партия' for _ in range(CANDIDATE_NUM))
@@ -81,19 +79,105 @@ df_candidates = pd.DataFrame({'name': candidate_name_series,
                               'photo': pd.Series('file_name.jpg' for _ in range(CANDIDATE_NUM)),  # название файла .jpg
                               })
 # print('df_candidates', df_candidates)
-df_candidates.to_sql(table_name_candidate, engine, if_exists='replace', index=False)
+candidates = df_candidates.to_dict('dict')
+print('df_candidates', candidates)
+
 
 table_name_uik = 'mainapp_uik'
 
 UIK_NUM = 3628
-df_uik = pd.DataFrame({'num_uik': pd.Series(i for i in range(1, UIK_NUM+1)),
-                       'num_tik': pd.Series(1 for _ in range(1, UIK_NUM+1)),
-                       'population': pd.Series(random.randint(100, 300) for _ in range(UIK_NUM)),
-                       'sum_numb_votes_fin': pd.Series(0 for _ in range(UIK_NUM)),
-                       'presence': pd.Series(0 for _ in range(UIK_NUM)),
-                       'perc_final_bul': pd.Series(0 for _ in range(UIK_NUM)),
-                       'bad_form': pd.Series(random.randint(0, 5) for _ in range(UIK_NUM)),
-                       'update_time': pd.Series('2020-05-16 08:36:38' for _ in range(UIK_NUM)),
-                       })
-# print('df_uik\n',df_uik)
-df_candidates.to_sql(table_name_candidate, engine, if_exists='append', index=False)
+
+
+def insert_account(account_name):
+    # print('account_name', account_name)
+    sql = """INSERT INTO accounts_account(name)
+             VALUES(%s) RETURNING id;"""
+    # print('sql', sql)
+    conn = None
+    account_id = None
+    try:
+        # Подключение к существующей базе данных
+        conn = psycopg2.connect(user="postgres",
+                                  # пароль, который указали при установке PostgreSQL
+                                  password="***",
+                                  host="huvalk.ru",
+                                  port="8001",
+                                  database="electorator")
+        # Курсор для выполнения операций с базой данных
+        cursor = conn.cursor()
+
+        # Распечатать сведения о PostgreSQL
+        print("Информация о сервере PostgreSQL")
+        print(conn.get_dsn_parameters(), "\n")
+        # Выполнение SQL-запроса
+        cursor.execute("SELECT version();")
+        # Получить результат
+        record = cursor.fetchone()
+        print("Вы подключены к - ", record, "\n")
+
+        cursor.execute(sql, (account_name,))
+
+
+        # get the generated id back
+        account_id = cursor.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cursor.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if conn:
+            conn.close()
+    return account_id
+
+
+def insert_candidate(name, party, info, sum_votes, photo):
+    # print('candidate_name', candidate_name)
+    sql = """INSERT INTO mainapp_candidate(name, party, info, sum_votes, photo)
+             VALUES(%s,%s,%s,%s,%s) RETURNING id;"""
+    # print('sql', sql)
+    conn = None
+    candidate_id = None
+    try:
+        # Подключение к существующей базе данных
+        conn = psycopg2.connect(user="postgres",
+                                  # пароль, который указали при установке PostgreSQL
+                                  password="***",
+                                  host="huvalk.ru",
+                                  port="8001",
+                                  database="electorator")
+        # Курсор для выполнения операций с базой данных
+        cursor = conn.cursor()
+
+        # Распечатать сведения о PostgreSQL
+        print("Информация о сервере PostgreSQL")
+        print(conn.get_dsn_parameters(), "\n")
+        # Выполнение SQL-запроса
+        cursor.execute("SELECT version();")
+        # Получить результат
+        record = cursor.fetchone()
+        print("Вы подключены к - ", record, "\n")
+
+        cursor.execute(sql, (name, party, info, sum_votes, photo,))
+
+        # get the generated id back
+        candidate_id = cursor.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cursor.close()
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if conn:
+            conn.close()
+    return candidate_id
+
+
+# print(insert_account('ac_name'))
+print(insert_candidate(candidates['name'][0], candidates['party'][0], candidates['info'][0], candidates['sum_votes'][0], candidates['photo'][0]))
+# PK_candadate = []
+# PK_candadate.append(insert_candidate(***))
