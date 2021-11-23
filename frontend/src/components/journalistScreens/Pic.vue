bd751a97dcb382210b50de657781c12761b107dc
Huvalk <akiraq@yandex.ru>
2021-11-23 20:46:03 +0300

    fix: demo

--- frontend/src/components/journalistScreens/Pic.vue
+++ frontend/src/components/journalistScreens/Pic.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="rounded-circle d-flex ans_avatar justify-content-center img-fluid">
-    <img :src="src"/>
+  <div class="d-flex rounded-circle wrap_avatar justify-content-center img-fluid">
+    <img class="ans_avatar" :src="src"/>
   </div>
 </template>
 
@@ -15,6 +15,10 @@ export default {
 
 <style scoped>
 .ans_avatar {
+    height: 20vh; /*180px;*/
+    width: 20vh;/*170px;*/
+}
+.wrap_avatar {
     height: 15vh; /*180px;*/
     width: 15vh;/*170px;*/
     overflow: hidden;
